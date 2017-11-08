fff0b75ae40d45f1e9e55e16ee60baac5dac8cac
Steven Senkowski <scsenkowski@gmail.com>
2017-11-08 15:16:16 +0000

    automatically backed up by learn

--- square_array.rb
+++ square_array.rb
@@ -1,7 +1,8 @@
 def square_array(array)
   my_array = []
-  array.each do |numbers|
-    i = numbers ** 2
-  my_array.push("i")
-end
+      array.each do |numbers|
+        i = numbers ** 2
+      my_array.push("i")
+      end
+  square_array(my_array)
 end
